891df1b0588062f87fb7a0cca976a17f6e7e4ae4
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-13 02:02:54 +0800

    add game

--- Program.java
+++ Program.java
@@ -2,21 +2,61 @@ import java.util.Random;
 
 public class Program {
     public static void main(String[] args) {
-        String icon = "🐶";
-
-        System.out.println(icon);
+        Game game = new Game();
+        char response;
+        do {
+            game.play();
+        } while (false);
     }
 }
 
 class Game {
-    private String data[] = {"🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐻‍❄️","🐨","🐯","🦁","🐮","🐷"};
+    private char data[] = {'A','B','C'};
+    private char picked[] = {'0','0','0'};
 
-    Game() {
-
+    private void generateTable(char picked[]) {
+        System.out.println("+---------+---------+---------+");
+        System.out.println("|         |         |         |");
+        System.out.println(String.format("|    %c    |    %c    |    %c    |",picked[0],picked[1],picked[2]));
+        System.out.println("|         |         |         |");
+        System.out.println("+---------+---------+---------+");
     }
-    public void generateTable() {
+
+    public void play(){
+        for(int i = 0; i < picked.length; i++) {
+            pick(i);
+        }
+    }
+
+    private void pick(int index){
+        Random random = new Random();
+        if(picked[index] != '0')return;
         
-    }
+        for (int i = 0; i <= 20; i++) {
+            generateTable(picked);
+            try {
+                Thread.sleep(20);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            int temp = random.nextInt(0, data.length);
+            picked[index] = data[temp];
+            if(i == 20)break;
+            clearConsole();
+        }
 
-    public void randomize(){}
+    }
+    
+    private void clearConsole() {
+        try {
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
